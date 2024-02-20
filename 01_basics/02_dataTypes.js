c16d89fd39fde499833582987888d21f5fd34833
ankitpandey1410 <125684719+ankitpandey1410@users.noreply.github.com>
2024-02-20 19:11:38 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict" // treat all js code as newer version
+
+// alert(3 + 4)    //  we are using nodejs not browser
+
+console.log(3 + 3)
+
+console.log("Ankit")
+
+
+
+let name = "Ankit"
+let age = 22
+let isLoggedIn = false
+
+//number => 2 to the power 53
+//bigint
+//string = ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
