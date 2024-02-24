6f64b77fd49817bdfa8035b216b3eb91c7046a8a
ankitpandey1410 <125684719+ankitpandey1410@users.noreply.github.com>
2024-02-24 08:51:29 +0000

    strings basic

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,34 @@
+// const name = "Ankit"
+// const repoCount = 100
+
+//console.log(name + repoCount + "value")
+
+//console.log(`Hello my name is ${name} and my repo cpount is ${repoCount}`)
+
+const gameName = new String('Ankit-kp-com')
+
+// console.log(gameName[0])
+// console.log(__proto__)
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(3))
+// console.log(gameName.indexOf('n'))
+
+// const newString = gameName.substring(0 , 4)
+// console.log(newString)
+
+// const anotherString = gameName.slice(-10 , 2)
+// console.log(anotherString)
+
+// const newStringOne = "    animal    "
+// console.log(newString)
+// console.log(newStringOne.trim())
+
+// const url = "https://ankit.com/ankit%20kumar"
+
+// console.log(url.replace('%20' , '-'))
+// console.log(url.includes('ankit'))
+// console.log(url.includes('pandey'))
+
+console.log(gameName.split('-'))
